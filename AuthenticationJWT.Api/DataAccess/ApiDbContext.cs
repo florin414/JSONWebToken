a4d53c28b34f3536a61f51487d9ad4bf35d2bf15
florin414 <magadan.florin.u3e@student.ucv.ro>
2023-10-20 20:32:11 +0300

    feat(DataAccess) add: Repositories (IRepository, UserRepository, RefreshTokenRepository) ApiDbContext

---
+++ AuthenticationJWT.Api/DataAccess/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthenticationJWT.Api.DataAccess;
+
+public class ApiDbContext : IdentityDbContext<User, IdentityRole<string>, string>
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.SeedUsers();
+    }
+}
---
+++ AuthenticationJWT.Api/DataAccess/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationJWT.Api.DataAccess.Repository;
+
+public interface IRepository<TEntity, TId> where TEntity : class
+{
+    public IQueryable<TEntity> GetAll();
+    public Task<TEntity> GetById(TId id);
+    public Task AddAsync(TEntity entity);
+    public Task Update(TEntity entity);
+    public Task RemoveAsync(TId entity);
+}
